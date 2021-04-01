82867d9bcae36ecd87a274cd83326b01c1f6db8f
xtovo <valeriy.onuchin@yandex.ru>
2021-04-01 20:32:55 +0300

    - sync

--- pclean.bat
+++ pclean.bat
@@ -4,7 +4,7 @@
 :: Clean output folders
 
 for /D /R %%i in (bin*) do (rd /s /q "%%i")
-for /D /R %%i in (obj*) do (rd /s /q "%%i")
+for /D /R %%i in (obj) do (rd /s /q "%%i")
 for /D /R %%i in (redist*) do (rd /s /q "%%i")
 for /D /R %%i in (debug*) do (rd /s /q "%%i")
 for /D /R %%i in (release*) do (rd /s /q "%%i")
